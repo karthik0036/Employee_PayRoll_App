a855bf6e0d69ccc33b6ce7f926970ed0c325c38f
Karthik <jeefkarthik036@gmail.com>
2022-05-21 15:38:37 +0530

    Created EmployeePayroll Spring Project

---
+++ main/java/com/example/EmployeePayRollApp/EmployeePayRollAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.EmployeePayRollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeePayRoll App");
+		SpringApplication.run(EmployeePayRollAppApplication.class, args);
+	}
+
+}
