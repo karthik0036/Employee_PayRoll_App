b2ad50c7d25a36f1f57af4aa25f13a231c52ec5a
Karthik <jeefkarthik036@gmail.com>
2022-05-24 13:42:21 +0530

    Added:Throwing  User friendly Exceptions if validation of name & salary fails

---
+++ main/java/com/example/EmployeePayRollApp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.example.EmployeePayRollApp.exceptions;
+
+public class EmployeePayrollException extends RuntimeException{
+    public EmployeePayrollException(String message){
+        super(message);
+    }
+}
